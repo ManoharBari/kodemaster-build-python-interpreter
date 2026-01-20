76a310ab2b6389d3131caa351becc26d0dfe74e2
Manohar Kale <manoharkale5368@gmail.com>
2026-01-20 17:46:56 +0530

    feat: set up project to read Python files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
-// Python Interpreter - Build Your Own!
-// This is your starting point. You'll add more code as you progress.
-
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int main(int argc, char* argv[]) {
-    std::cout << "Python Interpreter - Ready to build!\n";
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " [filename].py\n";
+        return 1;
+    }
+
+    const char *filename = argv[1];
+    std::ifstream inputFile(filename);
+
+    if (!inputFile)
+    {
+        std::cerr << "Error: could not open file '" << filename << "'\n";
+        return 1;
+    }
+
+    // Read entire file into a string using iterator trick
+    std::string source((std::istreambuf_iterator<char>(inputFile)), {});
+
+    std::cout << "Read " << source.length() << " bytes\n";
+
     return 0;
 }
