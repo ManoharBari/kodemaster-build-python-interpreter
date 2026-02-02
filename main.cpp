334aca048478767b23a3e1187fc3c79d55162e42
Manohar Kale <manoharkale5368@gmail.com>
2026-02-02 16:50:11 +0530

    feat: add interpreter foundation with Python objects

--- main.cpp
+++ main.cpp
@@ -31,13 +31,9 @@ int main(int argc, char *argv[])
         Lexer lexer(source);
         std::vector<Token> tokens = lexer.scanTokens();
 
-        // Parsing
-        Parser parser(tokens);
-        ProgramNode *program = parser.parse();
-
         // Interpreting
         Interpreter interpreter;
-        interpreter.interpret(program);
+        interpreter.interpret( parser.parse());
 
         // Cleanup
         delete program;
